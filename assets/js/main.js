f8286f2956b338c5506802680cb8beb2790ce85c
Jalexss <jhalx2198@gmail.com>
2021-05-16 09:31:45 -0500

    Add types

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,11 +19,9 @@ const getData = async (api) => {
 
 		console.log(arregloPokemon);
 		dibujarData(arregloPokemon);
-
 	} catch (error) {
 		console.log("Error", error);
 	}
-
 };
 
 //* Dibujar cards de Pokemones
@@ -32,13 +30,28 @@ const dibujarData = (data) => {
 	let html = "";
 	html += '<div class="row row-cols-1 row-cols-md-3 g-4">';
 	data.forEach((pokemon) => {
+		let tipo1 = pokemon.types[0].type.name;
+		let tipo2 = pokemon.types.length > 1 ? pokemon.types[1].type.name : "";
+
 		html += `
 	    <div class="col">
-	        <div class="card">
-	        <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
+	        <div class="card bg-transparent border-0">
+	        	<div class="img-contain rounded-circle bg-light">
+					 <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${ pokemon.name }">
+					 <div class="types d-flex justify-content-center text-capitalize text-white">
+					  	<div class = "bg-success rounded-pill px-2">${ tipo1} </div>
+						${ tipo2 && `<div class = "bg-info mx-2 rounded-pill px-2">${tipo2}</div>`}
+					</div>
+				</div>
 	        <div class="card-body text-center">
-	            <h5 class="card-title">${pokemon.name}</h5>
-	            <p class="card-text">Altura: ${Number(pokemon.height)/10}m <br>Peso: ${Number(pokemon.weight)/10}kg <br>Experiencia base: ${pokemon.base_experience}p </p>
+	            <h5 class="card-title text-uppercase text-danger fs-2">${
+								pokemon.name
+							}</h5>
+	            <p class="card-text text-white">Altura: ${
+								Number(pokemon.height) / 10
+							}m <br>Peso: ${
+			Number(pokemon.weight) / 10
+		}kg <br>Experiencia base: ${pokemon.base_experience}p </p>
 	        </div>
 	        </div>
 	    </div>
@@ -66,4 +79,3 @@ const paginacion = (data) => {
 //* Eejecutar getData
 
 getData(API);
-
